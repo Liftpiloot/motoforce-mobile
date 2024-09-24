bc8988e632cc8e6989bfa2703eee00d0fd437347
Abel <vandijkabel@gmail.com>
2024-09-24 10:09:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Hello World</h1>
+    </div>
+  </header>
+
+  <main>
+    <h1>Hello world</h1>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
