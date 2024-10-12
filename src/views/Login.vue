bed56d9b9ed42e261c06648ceb738d083f290f3c
Abel <vandijkabel@gmail.com>
2024-10-12 21:47:18 +0200

    Add login css and redirect login to /dashboard

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,162 @@
 <script setup>
 import { ref } from 'vue'
 import {API_URL} from "@/config.js";
+import InputField from "@/components/UI/InputField.vue";
+import StandardButton from "@/components/UI/StandardButton.vue";
 
 const username = ref('')
 const password = ref('')
 
+const errorMessage = ref('')
+const loading = ref(false)
+
 const login = async () => {
+  errorMessage.value = '';
+  loading.value = true;
   try {
     const response = await fetch(`${API_URL}/Login?identifier=${username.value}&password=${password.value}`);
+    if (response.status === 404) {
+      errorMessage.value = 'Username or password is incorrect';
+      return;
+    }
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error(await response.text());
     }
     const userData = await response.json();
     console.log('User data:', userData);
-    // Handle the user data as needed
+    localStorage.setItem('user', JSON.stringify(userData));
+    window.location.href = '/dashboard';
   } catch (error) {
-    console.error('There was a problem with the login request:', error);
+    errorMessage.value = 'An error occurred. Please try again';
+  } finally {
+    loading.value = false;
   }
 };
 </script>
 
 <template>
-  <div class="login-container">
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" v-model="username" required />
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+  <div class="window">
+    <div :class="['moto-force-icon', {loading}]">
+      <img src="../components/icons/moto-force-icon.svg" alt="Moto Force Icon"/>
+    </div>
+    <div class="login-container">
+        <div class="login-title">
+          <h1>Login</h1>
+        </div>
+        <form @submit.prevent="login" class="login-form">
+          <InputField v-model="username" :label="true" :label-text="'Name/Email:'"/>
+          <InputField v-model="password" :label="true" :label-text="'Password:'" type="password"/>
+          <StandardButton :type="'positive'" :content="'Login'"></StandardButton>
+        </form>
+        <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+    </div>
+    <div class="register-container">
+      <div>Don't have an account yet?</div>
+      <router-link to="/register" class="register-text">Register</router-link>
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+
+.window {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.625rem;
+  color: var(--Text-color);
+  font-weight: 800;
+  height: 100vh;
+}
+
+.moto-force-icon {
+  display: flex;
+  width: 22.5rem;
+  padding: 0.75rem 0rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  flex-shrink: 0;
+
+  img {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.625rem;
+  }
+}
+
+.moto-force-icon.loading img {
+  animation: neon 5s infinite;
+}
+
+@keyframes neon {
+  0% {
+    filter: hue-rotate(0deg);
+  }
+  50% {
+    filter: hue-rotate(360deg);
+  }
+  100% {
+    filter: hue-rotate(0deg);
+  }
+}
+
+.login-container {
+  display: flex;
+  padding: 3.125rem 0.625rem;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 75%;
+  max-width: 22.5rem;
+}
+
+.login-form {
+  display: flex;
+  padding: 0.625rem;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.625rem;
+  width: 100%;
+}
+
+.login-title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.625rem;
+  color: var(--Text-color);
+  text-align: center;
+  width: 100%;
+}
+
+.error-message {
+  color: var(--Text-color);
+  margin-top: 1rem;
+}
+
+.register-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.register-text {
+  all:unset;
+  background: -webkit-linear-gradient(68deg, #2200AC 11.4%, #7F2CBC 50.14%, #C900B8 88.87%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight: 700;
+  margin-left: 0.3125rem;
+  cursor: pointer;
+}
+
+.register-text:hover {
+  text-decoration: underline;
+}
+
+
+</style>
 
