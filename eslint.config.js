fcf1ea8ff7681c9e45c4ce63802f48f2f830754e
Abel <vandijkabel@gmail.com>
2025-01-17 14:55:51 +0100

    update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint, {parser} from "typescript-eslint";
+import tseslint, { parser } from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,vue}"],
     languageOptions: {
       globals: globals.browser,
-    },
-  },
-  pluginJs.configs.recommended,
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  {
-    files: ["**/*.vue"],
-    languageOptions: {
       parserOptions: {
         parser: parser,
       },
+    },
+    plugins: {
+      js: pluginJs,
+      vue: pluginVue,
+      ts: tseslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginVue.configs["flat/essential"].rules,
     },
   },
 ];
